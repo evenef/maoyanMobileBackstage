7c0e0cc91ad2d6965fe23cc7f21696ecd594bf08
even <even_ef@aliyun.com>
2017-08-07 14:01:05 +0800

    路由配置测试

--- backstage/src/main.js
+++ backstage/src/main.js
@@ -2,12 +2,15 @@ import Vue from 'vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-default/index.css'
 import App from './App.vue'
+
+import router from './router/router.js'
 // import store from "./store.js"
 
 Vue.use(ElementUI)
 
 new Vue({
 	el: "#app",
+	router,
 	// store,
 	template: `<App />`,
 	components: { App }
