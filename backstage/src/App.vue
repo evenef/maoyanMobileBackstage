de6036f27fca24d0193f3474d7873ab816112627
even <even_ef@aliyun.com>
2017-08-08 09:12:23 +0800

    修改主页按钮

--- backstage/src/App.vue
+++ backstage/src/App.vue
@@ -8,12 +8,11 @@
 		</el-col>
 	</el-row>
 	<el-row>
-		<el-col :span="6">
+		<el-col :span="4">
 			<el-col :span="24">
 				<el-menu mode="vertical" default-active="1" class="el-menu-vertical-demo" router >
 				<el-menu-item-group title="用户">
-					<el-menu-item index="1"><i class="el-icon-plus"></i>新增用户</el-menu-item>
-					<el-menu-item index="users"><i class="el-icon-star-on"></i>用户列表</el-menu-item>
+					<el-menu-item index="users"><i class="el-icon-star-on"></i>用户管理</el-menu-item>
 				</el-menu-item-group>
 				<el-menu-item-group title="电影">
 					<el-menu-item index="3"><i class="el-icon-plus"></i>新增电影</el-menu-item>
@@ -31,7 +30,7 @@
 				</el-menu>
 			</el-col>
 		</el-col>
-		<el-col :span="18">
+		<el-col :span="20">
 			<div class="content">
 				<router-view></router-view>
 			</div>
